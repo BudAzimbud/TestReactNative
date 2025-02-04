40901387192f46ac9b20cd28c741afc70d254977
azim <budazimbud@gmail.com>
2025-02-04 15:16:05 +0800

    add handling limit

--- src/action/list.ts
+++ src/action/list.ts
@@ -2,6 +2,6 @@ import { ApiResponseList, MusicTrack } from "../interface";
 import { api } from "../utils/api";
 
 export const getList = async ({term}: {term: string}) : Promise<ApiResponseList<MusicTrack>> => {
-  const response = await api.get<ApiResponseList<MusicTrack>>(`/search?term=${term}`);
+  const response = await api.get<ApiResponseList<MusicTrack>>(`/search?term=${term}&limit=25`);
   return response.data;
 };
